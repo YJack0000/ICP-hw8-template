bd49d09d2dc59a76e92f732cefb1df99e70fae22
YJack0000 <jackleagends@gmail.com>
2023-11-19 03:36:06 +0800

    chore: 直接把 main.c 固定

--- main.c
+++ main.c
@@ -5,23 +5,24 @@
 
 int main() {
     char op[10];
-    // TODO: Create browser with capacity 10
+    Browser b;
+    init_browser(&b, 1000);
 
     while(scanf("%s", op) != EOF) {
         if (strcmp(op, "new") == 0) {
-            // TODO
+            char url[100], title[100];
+            scanf("%s %s", url, title);
+            navigate_to(&b, create_webpage(url, title));
         } else if (strcmp(op, "back") == 0) {
-            // TODO
+            navigate_back(&b);
         } else if (strcmp(op, "forward") == 0) {
-            // TODO
+            navigate_forward(&b);
         } else if (strcmp(op, "current") == 0) {
             puts("---");
-            // TODO
+            print_current_webpage(&b);
             puts("---");
         } else {
             printf("Invalid operation\n");
         }
     }
-
-    // TODO: Destory browser
 }
